1bdabbe162671a07a24613306097c5425ad38d30
Salty Mat <mathew.zahedi@gmail.com>
2022-05-24 15:13:13 -0700

    asxdcvgb

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -9,6 +9,46 @@ function HTMLActuator() {
   this.score = 0;
 }
 
+function HTMLActuatorP2() {
+  this.tileContainer    = document.querySelector(".tile-containerP2");
+  // this.scoreContainer   = document.querySelector(".score-container");
+  // this.bestContainer    = document.querySelector(".best-container");
+  // this.messageContainer = document.querySelector(".game-message");
+
+  this.score = 0;
+}
+
+HTMLActuatorP2.prototype.actuate = function (grid, metadata) {
+  var self = this;
+
+  window.requestAnimationFrame(function () {
+    self.clearContainer(self.tileContainer);
+
+    grid.cells.forEach(function (column) {
+      column.forEach(function (cell) {
+        if (cell) {
+          self.addTile(cell);
+        }
+      });
+    });
+
+    console.log(grid)
+    socket.emit("message", grid);
+    self.updateScore(metadata.score);
+    self.updateBestScore(metadata.bestScore);
+
+    if (metadata.terminated) {
+      if (metadata.over) {
+        self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
+      }
+    }
+
+  });
+};
+
+
 HTMLActuator.prototype.actuate = function (grid, metadata) {
   var self = this;
 
